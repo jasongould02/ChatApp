83db1ccc48c886773b81aafa3af642d149bc3ea4
runiclp <15948550+runiclp@users.noreply.github.com>
2019-03-09 15:30:32 -0500

    Create MessagePrivacy.java

---
+++ src/jcg/java/chat/commons/MessagePrivacy.java
@@ -0,0 +1,10 @@
+package jcg.java.chat.commons;
+
+public enum MessagePrivacy {
+
+	PUBLIC, // Everyone can see the message
+	PRIVATE, // Only specified person can see the message
+	GROUP, // Only specified group can see the message
+	SERVER// Directed to only be parsed by the server? 
+	
+}

b89d14be5db2639a9f4b24d73e1bda251daca17a
runiclp <15948550+runiclp@users.noreply.github.com>
2019-05-06 15:46:46 -0400

    Created Connection.java

---
+++ src/jcg/java/chat/commons/connection/Connection.java
@@ -0,0 +1,124 @@
+package jcg.java.chat.commons.connection;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import jcg.java.chat.commons.log.Log;
+
+public class Connection {
+
+	//private String connectionName = "";
+	private Socket socket = null;
+	
+	private PrintWriter writer = null;
+	private BufferedReader reader = null;
+
+	public Connection(String ip, String port) {
+		if(connect(ip, port)) {
+			Log.log("Connection made to: ["+ip+":"+port+"]");
+			try {
+				writer = new PrintWriter(socket.getOutputStream());
+				reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public boolean closeConnection() {
+		try {
+			if(socket != null) {
+				if(writer != null) {
+					writer.flush();
+					writer.close();
+					writer = null;		//Log.log("PrintWriter closed successfully.");
+				}
+				if(reader != null) {
+					reader.close();
+					reader = null;		//Log.log("BufferedReader closed successfully.");
+				}
+				socket.close();
+				if(!socket.isClosed()) {
+					Log.error("Socket was unable to close. Setting it to null."); // Sett
+				} 
+				socket = null;
+				Log.log("Connection closed successfully. Socket and streams are closed/null.");
+				return true;
+			} else {
+				Log.log("Connection already closed.");
+				return true;
+			}
+		} catch (IOException e) {
+			Log.error("Failed to close connection.");
+			socket = null;
+			writer = null;
+			reader = null;
+			e.printStackTrace();
+			return false;
+		} 
+	}
+	
+	public boolean connect(String ip, String port) {
+		if(socket == null) {
+			socket = new Socket();
+		} else if(socket.isClosed()) {
+			socket = null;
+			socket = new Socket();
+		} else {
+			if(!socket.isClosed()) {
+				Log.log("Socket not yet closed. Disconnecting before making new connection");
+				closeConnection();
+			}
+			try {
+				socket.connect(new InetSocketAddress(ip, Integer.parseInt(port)));
+			} catch (NumberFormatException | IOException e) {
+				e.printStackTrace();
+				System.out.println("Failed to connect.");
+			}
+		}
+		
+		if(socket.isConnected()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void sendMessage(byte[] data) {
+		try {
+			socket.getOutputStream().write(data);
+			socket.getOutputStream().flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void sendMessage(String data) {
+		writer.print(data);
+		writer.flush();
+	}
+	
+	public Socket getSocket() {
+		return socket;
+	}
+	
+	/*private PrintWriter getPrintWriter() {
+		return writer;
+	}
+	
+	private BufferedReader getBufferedReader() {
+		return reader;
+	}
+	
+	private InputStream getInputStream() throws IOException {
+		return socket.getInputStream();
+	}
+	
+	private OutputStream getOutputStream() throws IOException {
+		return socket.getOutputStream();
+	}*/
+}
